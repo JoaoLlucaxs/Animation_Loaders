2966c70357edadb9200a223f5bd21b12b566da72
devjoao <joaolucasqueiriz@gmail.com>
2022-10-18 14:25:53 -0300

    loaders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import './sass/style.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+    <div class="box">
+      <div class="fading-lines"></div>
     </div>
+    <div class="box">
+      <div class="dots"></div>
+    </div>
+    <div class="box">
+      <div class="twin-circles"></div>
+    </div>
+    <div class="box">
+      <div class="wave"></div>
+    </div>
+    <div class="box">
+      <div class="circle"></div>
+    </div>
+    <div class="box">
+      <div class="bouncing-bar">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+      </div>
+    </div>
+    <div class="box">
+      <div class="focus"></div>
+    </div>
+    <div class="box">
+      <div class="spinner-dots"></div>
+    </div>
+  </div>
   );
 }
 
